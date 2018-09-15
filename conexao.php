f2edfcd684c6e78d4173ffd4981df9703234da4f
Augustompa <augustompa>
2018-09-15 11:27:21 -0300

    Versao2.0

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:serverbdtestepratico.database.windows.net,1433; Database = Bd_testepratico", "augustompa", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:serverbdtestepratico.database.windows.net,1433; Database = Bd_testepratico", "augustompa", "Augustoa08u90");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -9,7 +9,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "augustompa@serverbdtestepratico", "pwd" => "{your_password_here}", "Database" => "Bd_testepratico", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "augustompa@serverbdtestepratico", "pwd" => "Augustoa08u90", "Database" => "Bd_testepratico", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:serverbdtestepratico.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
