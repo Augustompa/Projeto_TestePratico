35e4c5669d10521e50ba85e8626bd924b344053c
Augustompa <augustompa>
2018-09-15 12:24:40 -0300

    versao 3.0

--- conexao.php
+++ conexao.php
@@ -1,15 +1,18 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha="";
-    $dbname = "bd_testepratico";
-    $conn = mysqli_connect($servidor, $usuario,$senha,$dbname);
+    $conn = new PDO("sqlsrv:server = tcp:serverbdtestepratico.database.windows.net,1433; Database = Bd_testepratico", "augustompa", "Augustoa08u90");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "augustompa@serverbdtestepratico", "pwd" => "Augustoa08u90", "Database" => "Bd_testepratico", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:serverbdtestepratico.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
 
--- salva_conteudo.php
+++ salva_conteudo.php
@@ -15,7 +15,7 @@ $status= $_POST ['status'];
 $Agencia= $_POST ['Agencia'];
 $conta= $_POST ['conta'];
 
-$result_msg_contato = "INSERT INTO dbo.empresa (cnpj,razao_social,nome_fantasia,email,endereco,cidade,estado,telefone,Categoria,status_cadastro,Agencia,conta) VALUES ('$cnpj','$razaosocial',$nomefantasia,'$email','$endereco','$cidade','$estado','$telefone','$Categoria','$status','$Agencia','$conta', NOW ())";
+$result_msg_contato = "INSERT INTO empresa (cnpj,razao_social,nome_fantasia,email,endereco,cidade,estado,telefone,Categoria,status_cadastro,Agencia,conta) VALUES ('$cnpj','$razaosocial',$nomefantasia,'$email','$endereco','$cidade','$estado','$telefone','$Categoria','$status','$Agencia','$conta', NOW ())";
 
-$resultado_msg_contato = mysqli_query($conn, $result_msg_contato);
+$resultado_msg_contato = sqlsrv_query($conn, $result_msg_contato);
 ?>
