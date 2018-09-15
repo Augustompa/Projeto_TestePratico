50805ca5d3c8283b6e037c0d567a6a773ae329b4
Augustompa <augustompa>
2018-09-15 12:13:59 -0300

    versao bd local

--- salva_conteudo.php
+++ salva_conteudo.php
@@ -15,7 +15,7 @@ $status= $_POST ['status'];
 $Agencia= $_POST ['Agencia'];
 $conta= $_POST ['conta'];
 
-$msg_contato = "INSERT INTO dbo.empresa (cnpj,razao_social,nome_fantasia,email,endereco,cidade,estado,telefone,Categoria,status_cadastro,Agencia,conta) VALUES ('$cnpj','$razaosocial',$nomefantasia,'$email','$endereco','$cidade','$estado','$telefone','$Categoria','$status','$Agencia','$conta', NOW ())";
+$result_msg_contato = "INSERT INTO dbo.empresa (cnpj,razao_social,nome_fantasia,email,endereco,cidade,estado,telefone,Categoria,status_cadastro,Agencia,conta) VALUES ('$cnpj','$razaosocial',$nomefantasia,'$email','$endereco','$cidade','$estado','$telefone','$Categoria','$status','$Agencia','$conta', NOW ())";
 
-$msg_contato = mysqli_connect($conn, $msg_contato);
+$resultado_msg_contato = mysqli_query($conn, $result_msg_contato);
 ?>
